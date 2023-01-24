f311790ff478a2a94688f8234084bb7c91147a63
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-01-24 15:43:05 -0500

    position player controls

--- src/components/PlayerControls/PlayerControls.jsx
+++ src/components/PlayerControls/PlayerControls.jsx
@@ -1,6 +1,5 @@
 import style from "./PlayerControls.module.css";
 import useTimeout from "../../hooks/useTimeout";
-import { useState } from "react";
 
 function PlayerControls({ handleExit, handleNext, handlePause, handlePlay }) {
   const { isTrue, setIsTrue } = useTimeout(3000);
@@ -14,21 +13,19 @@ function PlayerControls({ handleExit, handleNext, handlePause, handlePlay }) {
   return (
     <div onMouseMove={handleMove} className={style.listener}>
       {isTrue && (
-        <div className={style.container}>
-          <div className={style.position}>
-            <button className={style.button} onClick={handlePlay}>
-              play
-            </button>
-            <button className={style.button} onClick={handlePause}>
-              pause
-            </button>
-            <button className={style.button} onClick={handleNext}>
-              skip
-            </button>
-            <button className={style.button} onClick={handleExit}>
-              exit
-            </button>
-          </div>
+        <div className={style.position}>
+          <button className={style.button} onClick={handlePlay}>
+            play
+          </button>
+          <button className={style.button} onClick={handlePause}>
+            pause
+          </button>
+          <button className={style.button} onClick={handleNext}>
+            skip
+          </button>
+          <button className={style.button} onClick={handleExit}>
+            exit
+          </button>
         </div>
       )}
     </div>
