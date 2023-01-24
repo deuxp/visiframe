75024094622d8a5b43c121222cbda11f4cea45f1
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-01-24 14:16:07 -0500

    wip window listener classnames

--- src/components/PlayerControls/PlayerControls.jsx
+++ src/components/PlayerControls/PlayerControls.jsx
@@ -1,22 +1,31 @@
 import style from "./PlayerControls.module.css";
-import React from "react";
+import { useState } from "react";
 
 function PlayerControls({ handleExit, handleNext, handlePause, handlePlay }) {
+  const [hidden, setHidden] = useState(false);
+
+  const handleClick = e => {
+    e.target.classList.toggle("hidden");
+    console.log(e.target.classList);
+  };
+
   return (
-    <div className={style["container__button"]}>
-      <div className={style["button--position"]}>
-        <button className={style["button"]} onClick={handlePlay}>
-          play
-        </button>
-        <button className={style["button"]} onClick={handlePause}>
-          pause
-        </button>
-        <button className={style["button"]} onClick={handleNext}>
-          skip
-        </button>
-        <button className={style["button"]} onClick={handleExit}>
-          exit
-        </button>
+    <div onClick={handleClick} className={style["window--listener"]}>
+      <div className={style[`container__button ${hidden ? "hidden" : ""}`]}>
+        <div className={style["button--position"]}>
+          <button className={style["button"]} onClick={handlePlay}>
+            play
+          </button>
+          <button className={style["button"]} onClick={handlePause}>
+            pause
+          </button>
+          <button className={style["button"]} onClick={handleNext}>
+            skip
+          </button>
+          <button className={style["button"]} onClick={handleExit}>
+            exit
+          </button>
+        </div>
       </div>
     </div>
   );
