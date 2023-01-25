a91d24abdd405be4d2dbbd8f70f2ca2a3652352b
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-01-25 01:53:18 -0500

    hide mouse on no move

--- src/components/PlayerControls/PlayerControls.jsx
+++ src/components/PlayerControls/PlayerControls.jsx
@@ -1,9 +1,14 @@
 import style from "./PlayerControls.module.css";
 import useTimeout from "../../hooks/useTimeout";
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 
 function PlayerControls({ handleExit, handleNext, handlePause, handlePlay }) {
   const { isTrue, setIsTrue } = useTimeout(3000);
+  const listener = useRef(null);
+
+  useEffect(() => {
+    listener.current.style.cursor = !isTrue ? "none" : "default";
+  }, [isTrue]);
 
   const handleMove = e => {
     if (!isTrue) {
@@ -12,7 +17,7 @@ function PlayerControls({ handleExit, handleNext, handlePause, handlePlay }) {
   };
 
   return (
-    <div onMouseMove={handleMove} className={style.listener}>
+    <div onMouseMove={handleMove} ref={listener} className={style.listener}>
       {isTrue && (
         <div className={style.position}>
           <button className={style.button} onClick={handlePlay}>
