424ef5575a7f69a40c1d0abf06cf4c6b02cf040b
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-02-14 13:14:29 -0500

    button svg icons

--- src/components/PlayButtons/PlayButtons.jsx
+++ src/components/PlayButtons/PlayButtons.jsx
@@ -1,11 +1,12 @@
 import style from "./PlayButtons.module.css";
+import { play, pause, skip } from "./icons";
 
 function PlayButtons({ handleNext, handlePlay, handlePause }) {
   return (
     <div className={style.button__group}>
-      <button onClick={handlePlay}>play</button>
-      <button onClick={handlePause}>pause</button>
-      <button onClick={handleNext}>skip</button>
+      <button onClick={handlePlay}>{play}</button>
+      <button onClick={handlePause}>{pause}</button>
+      <button onClick={handleNext}>{skip}</button>
     </div>
   );
 }
---
+++ src/components/PlayButtons/icons.js
@@ -0,0 +1,65 @@
+const play = (
+  <svg
+    // width="36"
+    height="16"
+    viewBox="0 0 36 42"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M34.5 18.4019C36.5 19.5566 36.5 22.4434 34.5 23.5981L4.5 40.9186C2.5 42.0733 -1.99455e-06 40.6299 -1.8936e-06 38.3205L-3.79393e-07 3.67949C-2.78445e-07 1.37009 2.5 -0.0732867 4.5 1.08141L34.5 18.4019Z"
+      fill="white"
+    />
+  </svg>
+);
+
+const skip = (
+  <svg
+    // width="30"
+    height="16"
+    viewBox="0 0 95 52"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      opacity="1"
+      x="83.5"
+      y="6.5"
+      width="11"
+      height="39"
+      rx="2.5"
+      fill="white"
+      stroke="white"
+    />
+    <path
+      d="M81.5 23.4019C83.5 24.5566 83.5 27.4434 81.5 28.5981L51.5 45.9186C49.5 47.0733 47 45.6299 47 43.3205L47 8.67949C47 6.37009 49.5 4.92671 51.5 6.08141L81.5 23.4019Z"
+      fill="white"
+    />
+    <path
+      d="M47.5 23.4019C49.5 24.5566 49.5 27.4434 47.5 28.5981L17.5 45.9186C15.5 47.0733 13 45.6299 13 43.3205L13 8.67949C13 6.37009 15.5 4.92671 17.5 6.08141L47.5 23.4019Z"
+      fill="white"
+    />
+  </svg>
+);
+
+const pause = (
+  <svg
+    // width="32"
+    height="16"
+    viewBox="0 0 32 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect opacity="1" width="14.3448" height="40" rx="3" fill="white" />
+    <rect
+      opacity="1"
+      x="17.6552"
+      width="14.3448"
+      height="40"
+      rx="3"
+      fill="white"
+    />
+  </svg>
+);
+
+export { play, pause, skip };
