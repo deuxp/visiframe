56c5081d0571fc99cade60a4a730301ce30b4b97
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-03-07 16:00:52 -0500

    validate channel

--- public/preload.js
+++ public/preload.js
@@ -1,27 +1,36 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 const indexBridge = {
-  getEmbeds: (selection, cb) => {
+  getEmbeds: (channel, selection, cb) => {
+    let validChannel = ["embeddedVideoList"];
     ipcRenderer.invoke("getEmbeds", selection);
-    const listener = ipcRenderer.on("embeddedVideoList", (event, response) => {
-      try {
-        const json = JSON.parse(response);
-        cb(json);
-        listener.removeAllListeners("embeddedVideoList");
-      } catch (error) {
-        cb([]);
-        listener.removeAllListeners("embeddedVideoList");
-      }
-    });
+    if (validChannel.includes(channel)) {
+      const listener = ipcRenderer.on(
+        "embeddedVideoList",
+        (event, response) => {
+          try {
+            const json = JSON.parse(response);
+            cb(json);
+            listener.removeAllListeners("embeddedVideoList");
+          } catch (error) {
+            cb([]);
+            listener.removeAllListeners("embeddedVideoList");
+          }
+        }
+      );
+    }
   },
-  getMenuItems: cb => {
-    ipcRenderer.invoke("getMenuItems");
-    const listener = ipcRenderer.on("sendMenuItems", (e, response) => {
-      const json = JSON.parse(response);
-      console.log(json);
-      cb(json);
-      listener.removeAllListeners("sendMenuItems");
-    });
+  getMenuItems: (channel, cb) => {
+    let validChannel = ["sendMenuItems"];
+    if (validChannel.includes(channel)) {
+      ipcRenderer.invoke("getMenuItems");
+      const listener = ipcRenderer.on("sendMenuItems", (e, response) => {
+        const json = JSON.parse(response);
+        console.log(json);
+        cb(json);
+        listener.removeAllListeners("sendMenuItems");
+      });
+    }
   },
   setWindowsize: size => {
     ipcRenderer.invoke("resizeWindow", size);
