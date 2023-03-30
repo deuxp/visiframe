b2c491a445f030ff620c2128c0bbba80ff8b86fb
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-03-30 01:31:00 -0400

    forge ahead

--- forge.config.js
+++ forge.config.js
@@ -1,21 +1,31 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    osxSign: {
+      entitlements: "./entitlements.plist",
+    },
+    osxNotarize: {
+      tool: "notarytool",
+      appleId: process.env.REACT_APP_APPLE_ID,
+      appleIdPassword: process.env.REACT_APP_APPLE_ID_PASSWORD,
+      teamId: process.env.REACT_APP_TEAM_ID,
+    },
+  },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
