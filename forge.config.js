dedc505142c7dce280767183e49fafc56f6f8266
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-03-30 07:25:28 -0400

    forge config

--- forge.config.js
+++ forge.config.js
@@ -1,12 +1,16 @@
 module.exports = {
   packagerConfig: {
     osxSign: {
-      entitlements: "./entitlements.plist",
+      identity: process.env.REACT_APP_APPLE_ID,
+      "hardened-runtime": true,
+      entitlements: "entitlements.plist",
+      "entitlements-inherit": "entitlements.plist",
+      "signature-flags": "library",
     },
     osxNotarize: {
       tool: "notarytool",
-      appleId: process.env.REACT_APP_APPLE_ID,
-      appleIdPassword: process.env.REACT_APP_APPLE_ID_PASSWORD,
+      appleId: process.env.REACT_APP_APP_ID,
+      appleIdPassword: process.env.REACT_APP_APP_ID_PASSWORD,
       teamId: process.env.REACT_APP_TEAM_ID,
     },
   },
