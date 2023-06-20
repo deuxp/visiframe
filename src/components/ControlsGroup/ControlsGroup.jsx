7a7e17c76a74143b3c8765c1188c3d6603b11fd6
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-06-20 11:00:50 -0400

    fullscreen button

--- src/components/ControlsGroup/ControlsGroup.jsx
+++ src/components/ControlsGroup/ControlsGroup.jsx
@@ -1,3 +1,5 @@
+import { exitFullscreen, fullscreen } from "../PlayButtons/icons";
+import { useState } from "react";
 import style from "./ControlsGroup.module.css";
 import useTimeout from "../../hooks/useTimeout";
 import { useEffect, useRef } from "react";
@@ -11,8 +13,24 @@ function ControlsGroup({
   menu,
   handleSelection,
 }) {
+  // DISAPPEAR
+  /** Disappear the controls group */
   const { isTrue, setIsTrue } = useTimeout(6000);
   const controlsGroup = useRef(null);
+
+  /** Toggle full screen */
+  const [fullscreenToggle, setFullscreenToggle] = useState(false);
+  function makeFullscreen() {
+    const isFullscreen = document.fullscreenElement;
+    if (isFullscreen) {
+      document.exitFullscreen();
+      setFullscreenToggle(false);
+    }
+    if (!isFullscreen) {
+      document.body.requestFullscreen();
+      setFullscreenToggle(true);
+    }
+  }
 
   useEffect(() => {
     controlsGroup.current.style.cursor = !isTrue ? "none" : "default";
@@ -30,14 +48,19 @@ function ControlsGroup({
       className={style.container}
     >
       {isTrue && (
-        <div className={style.controls__group}>
-          <Dropup menu={menu} handleSelection={handleSelection} />
-          <PlayButtons
-            handleNext={handleNext}
-            handlePlay={handlePlay}
-            handlePause={handlePause}
-          />
-        </div>
+        <>
+          <div className={style.controls__group}>
+            <Dropup menu={menu} handleSelection={handleSelection} />
+            <PlayButtons
+              handleNext={handleNext}
+              handlePlay={handlePlay}
+              handlePause={handlePause}
+            />
+          </div>
+          <button className={style.button} onClick={makeFullscreen}>
+            {fullscreenToggle ? exitFullscreen : fullscreen}
+          </button>
+        </>
       )}
     </div>
   );
--- src/components/PlayButtons/PlayButtons.jsx
+++ src/components/PlayButtons/PlayButtons.jsx
@@ -2,21 +2,11 @@ import style from "./PlayButtons.module.css";
 import { play, pause, skip } from "./icons";
 
 function PlayButtons({ handleNext, handlePlay, handlePause }) {
-  function fullscreen() {
-    const isFullscreen = document.fullscreenElement;
-    if (isFullscreen) {
-      document.exitFullscreen();
-    }
-    if (!isFullscreen) {
-      document.body.requestFullscreen();
-    }
-  }
   return (
     <div className={style.button__group}>
       <button onClick={handlePlay}>{play}</button>
       <button onClick={handlePause}>{pause}</button>
       <button onClick={handleNext}>{skip}</button>
-      <button onClick={fullscreen}>{"fullscreen"}</button>
     </div>
   );
 }
